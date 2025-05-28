941660d8de90b7c2c95fec063bad60f5ed64958d
ZeroDav808 <199318346+ZeroDav808@users.noreply.github.com>
2025-05-27 19:17:26 -0500

    Changes

--- index.js
+++ index.js
@@ -1,46 +1,80 @@
-const add = (x,y) => {
-    return x + y;
-}
+const add = (x, y) => {
+  return x + y;
+};
 
-const sub = (x,y) => {
-    return x - y;
-}
+const sub = (x, y) => {
+  return x - y;
+};
 
-const multiply = (x,y) => {
-    return x * y;
-}
+const multiply = (x, y) => {
+  return x * y;
+};
 
-const divide = (x,y) => {
-    if(y === 0){
-        return 'Error';
-    } else{
-        return x  / y;
-    }
-}
-
+const divide = (x, y) => {
+  if (y === 0) {
+    return "Error";
+  } else {
+    return x / y;
+  }
+};
 
 function operate(x, y, operation) {
-    switch(operation){
-        case '+':
-            return add(x,y);
-        case '-':
-            return sub(x,y);
-        case '*':
-            return multiply(x,y);
-        case '/':
-            return divide(x,y);
-        default:
-            return '????';
+  switch (operation) {
+    case "+":
+      return add(x, y);
+    case "-":
+      return sub(x, y);
+    case "*":
+      return multiply(x, y);
+    case "/":
+      return divide(x, y);
+    default:
+      return "????";
+  }
+}
+
+const values = {
+  num1 : null,
+  num2 : null,
+  operation1 : null,
+  operation2 : null,
+  result : null,
+}
+
+
+
+const keyboard = document.querySelector(".keyboard");
+const output = document.querySelector("p");
+
+function clear() {
+    let num1 = undefined;
+    let num2 = undefined;
+    let operation1 = undefined;
+    let operation2 = undefined;
+    output.textContent = "";
+}
+
+function handleNumber(element){
+  if()
+}
+
+function handleClick(event) {
+    const clickedElement = event.target;
+
+    switch(clickedElement.className){
+      case 'number':
+        handleNumber(clickedElement);
+        break;
+      case 'operation':
+
+      case 'equal':
+
+      case 'clear':
+
+
     }
 }
 
-let num1 = undefined;
-let num2 = undefined;
-let operation = undefined;
-const keyboard = document.querySelector('.keyboard');
-
-function handleClick(e) {
-    console.log(e.target.textContent);
-}
-
-keyboard.addEventListener('click', (e) => {handleClick(e)});
+keyboard.addEventListener("click", (e) => {
+  handleClick(e);
+});
