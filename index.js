9043cab1c2316d25572fff845cb299bbbcd49174
ZeroDav808 <199318346+ZeroDav808@users.noreply.github.com>
2025-05-29 11:59:41 -0500

    Added ability to handle numbers and operations:

--- index.js
+++ index.js
@@ -32,3 +32,65 @@ function operate(x, y, operation) {
       return "????";
   }
 }
+
+const screen = document.querySelector('p');
+const keyboard = document.querySelector('.keyboard');
+
+let num1 = null;
+let num2 = null;
+let op1 = null;
+// let op2 = null;
+let result = null;
+
+function clear() {
+  num1 = null;
+  num2 = null;
+  op1 = null;
+  //op2 = null;
+  result = null;
+  screen.textContent = '';
+}
+
+function handleNumber(value) {
+  if(!op1){
+    if(!num1){
+      num1 = value;
+    }else{
+      num1 += value;
+    }
+    screen.textContent = num1;
+  }
+}
+
+function handleOperation(value) {
+  op1 = value;
+  console.log(op1);
+}
+
+function handleClick(e) {
+  const button = e.target;
+
+  switch (button.className) {
+    case 'number':
+      console.log('The button is a number');
+      handleNumber(button.textContent);
+      break;
+    case 'operation':
+      console.log('The button is an operation');
+      handleOperation(button.textContent);
+      break;
+    case 'equal':
+      console.log('The button is an equal button');
+      break;
+    case 'clear':
+      console.log('The button is a clear button');
+      clear();
+      break;
+  }
+
+}
+
+
+keyboard.addEventListener('click', (e) => {
+  handleClick(e);
+})
