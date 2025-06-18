204086e707b2a19e6804b58ae4d9bd16b5c2020c
ZeroDav808 <199318346+ZeroDav808@users.noreply.github.com>
2025-06-18 15:17:57 -0500

    Created operate function that will perform and return desired output of calculation

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-function add(x,y){
-    return x + y;
+function add(x, y) {
+  return x + y;
 }
 
-function sub(x,y){
-    return x - y;
+function sub(x, y) {
+  return x - y;
 }
 
-function multiply(x,y){
-    return x * y;
+function multiply(x, y) {
+  return x * y;
 }
 
-function divide(x,y){
-    if(y === 0){
-        return `Error!!! Can't divide by zero.`
-    }
-    return x / y;
+function divide(x, y) {
+  if (y === 0) {
+    return `Error!!! Can't divide by zero.`;
+  }
+  return x / y;
+}
+
+const opVariables = {
+  firstOperand: null,
+  secondOperand: null,
+  operation: null,
+};
+
+function operate(num1, operation, num2) {
+  switch (operation) {
+    case "+":
+      console.log(`The result of ${num1} + ${num2} = ${add(num1, num2)}`);
+      break;
+    case "-":
+      console.log(`The result of ${num1} - ${num2} = ${sub(num1, num2)}`);
+      break;
+    case "*":
+      console.log(`The result of ${num1} * ${num2} = ${multiply(num1, num2)}`);
+      break;
+    case "/":
+      console.log(`The result of ${num1} / ${num2} = ${divide(num1, num2)}`);
+      break;
+  }
 }
