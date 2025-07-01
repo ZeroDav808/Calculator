afafeeb0754b70427961a2142a47e22927cc069f
ZeroDav808 <199318346+ZeroDav808@users.noreply.github.com>
2025-07-01 16:14:15 -0500

    Added ability to store numbers in variables and on screen

--- index.js
+++ index.js
@@ -34,11 +34,82 @@ function operate(num1, operation, num2) {
   }
 }
 
-const keypad = document.querySelector('#inputs');
-const display = document.querySelector('#display');
+let values = {
+  firstNum: null,
+  secondNum: null,
+  operator: null,
+};
 
+function clear() {
+  for (let key in values) {
+    values[key] = null;
+  }
+  screen.textContent = "";
+}
 
-keypad.addEventListener('click', (event)=> {
-  display.textContent = event.target.textContent;
-  console.log(event.target.textContent);
-})
+function handleNumber(value) {
+  const num = value.textContent;
+
+  if (!values.operator) {
+    if (!values.firstNum) {
+      values.firstNum = num;
+    } else {
+      values.firstNum += num;
+    }
+    screen.textContent = values.firstNum;
+  } else {
+    if (!values.secondNum) {
+      values.secondNum = num;
+    } else {
+      values.secondNum += num;
+      screen.textContent = values.secondNum;
+    }
+  }
+}
+
+function handleOperator(target) {
+  /* 
+  Case that need to be handled:
+  1. If the operator is entered but there is no second number and no result
+    - We just need to store the operator and allow the user to enter the second
+      number. If they want to chain operators then the next case will handle that,
+      else they should just enter equal to calculate the result.
+      
+  2. If the operator is entered and there is two numbers already
+    - When two numbers are present and another operator is entered,
+      we should calculate a result, set the first number = result, 
+      and allow for the second number to be entered and chained
+      like previously
+  */
+}
+
+function handleClick(event) {
+  let target = event.target;
+
+  switch (target.classList[1]) {
+    case "number":
+      console.log(`It's a number`);
+      handleNumber(target);
+      break;
+    case "operator":
+      console.log(`It's an operator`);
+      handleOperator(target);
+      break;
+    case "equal":
+      console.log(`It's an equal sign`);
+      break;
+    case "clear":
+      console.log(`It's a clear sign`);
+      clear();
+      break;
+    default:
+      "???";
+  }
+}
+
+const keyboard = document.querySelector("#inputs");
+const screen = document.querySelector("#display");
+
+keyboard.addEventListener("click", (event) => {
+  handleClick(event);
+});
