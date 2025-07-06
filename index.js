7c59e1f6cb6de2a8f09568f3fb40319a2b74cad3
ZeroDav808 <199318346+ZeroDav808@users.noreply.github.com>
2025-07-06 18:04:15 -0500

    Made some formatting changes

--- index.js
+++ index.js
@@ -14,85 +14,58 @@ function divide(x, y) {
   if (y === 0) {
     return `Error!!! Can't divide by zero.`;
   }
+
   return x / y;
 }
+
+//----------------------------------------------------------------------------------------------------------------
 
 function operate(num1, operation, num2) {
   switch (operation) {
     case "+":
-      console.log(`The result of ${num1} + ${num2} = ${add(num1, num2)}`);
-      break;
+      console.log(`The result of ${num1} + ${num2} = ${add(+num1, +num2)}`);
+      return add(+num1, +num2);
     case "-":
-      console.log(`The result of ${num1} - ${num2} = ${sub(num1, num2)}`);
-      break;
+      console.log(`The result of ${num1} - ${num2} = ${sub(+num1, +num2)}`);
+      return sub(+num1, +num2);
     case "*":
-      console.log(`The result of ${num1} * ${num2} = ${multiply(num1, num2)}`);
-      break;
+      console.log(`The result of ${num1} * ${num2} = ${multiply(+num1, +num2)}`);
+      return multiply(+num1, +num2);
     case "/":
-      console.log(`The result of ${num1} / ${num2} = ${divide(num1, num2)}`);
-      break;
+      console.log(`The result of ${num1} / ${num2} = ${divide(+num1, +num2)}`);
+      return divide(+num1, +num2);
+    default:
+      return "???";
   }
 }
 
+//---------------------------------------------------------------------------------------------------------------
+
 let values = {
   firstNum: null,
+
   secondNum: null,
+
   operator: null,
+
   result: null,
 };
 
 function clear() {
   for (let key in values) {
     console.log(`${key} is now null`);
+
     values[key] = null;
   }
+
   screen.textContent = "";
 }
 
-function handleNumber(value) {
-  const num = value.textContent;
+function handleNumber(value) {}
 
-  if (!values.operator) {
-    if (!values.firstNum) {
-      values.firstNum = num;
-    } else {
-      values.firstNum += num;
-    }
-    screen.textContent = values.firstNum;
-  } else {
-    if (!values.secondNum) {
-      values.secondNum = num;
-      screen.textContent = values.secondNum;
-    } else {
-      values.secondNum += num;
-      screen.textContent = values.secondNum;
-    }
-  }
-}
+function handleOperator(target) {}
 
-function handleOperator(target) {
-  /* 
-  Case that need to be handled:
-  1. If the operator is entered but there is no second number and no result
-    - We just need to store the operator and allow the user to enter the second
-      number. If they want to chain operators then the next case will handle that,
-      else they should just enter equal to calculate the result.
-
-  2. If the operator is entered and there is two numbers already
-    - When two numbers are present and another operator is entered,
-      we should calculate a result, set the first number = result, 
-      and allow for the second number to be entered and chained
-      like previously
-  */
-
-  const op = target.textContent;
-  // Case 1 Implementation
-  if (!values.result && !values.secondNum && values.firstNum) {
-    values.operator = op;
-    screen.textContent = "";
-    console.log(values.operator);
-  }
-}
+function handleEqual() {}
 
 function handleClick(event) {
   let target = event.target;
@@ -100,7 +73,6 @@ function handleClick(event) {
   switch (target.classList[1]) {
     case "number":
       console.log(`It's a number`);
-      handleNumber(target);
       break;
     case "operator":
       console.log(`It's an operator -> ${target.textContent}`);
@@ -108,6 +80,7 @@ function handleClick(event) {
       break;
     case "equal":
       console.log(`It's an equal sign`);
+      handleEqual();
       break;
     case "clear":
       console.log(`It's a clear sign`);
@@ -121,6 +94,7 @@ function handleClick(event) {
 const keyboard = document.querySelector("#inputs");
 const screen = document.querySelector("#display");
 
+console.log(`Keyboard activated. Attaching event listener to keyboard.`)
 keyboard.addEventListener("click", (event) => {
   handleClick(event);
 });
